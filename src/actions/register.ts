42adc88be225def423646e434cbf58552886db5b
Rubens Henrique Pereira <rubensh206@gmail.com>
2024-03-22 13:18:54 -0300

    Add authentication routes and user data handling

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,13 +2,35 @@
 
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import bcryptjs from "bcryptjs";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
 
   if (!validateFields.success) {
-    return { error: "Invalid fields" };
+    return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent" };
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcryptjs.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // Send email
+
+  return { success: "User created!" };
 };
